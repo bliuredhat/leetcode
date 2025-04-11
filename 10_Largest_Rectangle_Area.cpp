f828b773a095f227388db8007cd6504e888f726b
bo.liu <bliuredhat@gmail.com>
2025-04-11 16:08:40 +0800

    接雨水

--- 10_Largest_Rectangle_Area.cpp
+++ 10_Largest_Rectangle_Area.cpp
@@ -2,7 +2,7 @@
  * @Author: bliuredhat@gmail.com
  * @Date: 2023-07-17 16:18:18
  * @LastEditors: bliuredhat@gmail.com
- * @LastEditTime: 2025-04-11 11:38:55
+ * @LastEditTime: 2025-04-11 16:07:30
  * @Description: 
  */
 #include <vector>
@@ -15,6 +15,7 @@ int LargestRectangleArea(const std::vector<int>& vec) {
         return res;
     }
     std::stack<int> st;
+    st.push(-1);
     int size = vec.size();
     for(int idx=0; idx < size; idx++) {
         int val = vec[idx];
@@ -26,26 +27,33 @@ int LargestRectangleArea(const std::vector<int>& vec) {
         }
         st.push(idx);
     }
+     while (st.top() != -1) {
+        int height = vec[st.top()];
+        st.pop();
+        int width = vec.size() - st.top() - 1;
+        res = std::max(res, height * width);
+    }
     return res;
 }
 
 
-int maxAreaV2(std::vector<int>& arr) {
-        arr.push_back(0);
+int largestWater(std::vector<int>& arr) {
+        //arr.push_back(0);
         int n = arr.size();
         int ans = 0;
 
         std::stack<int> st;
         for (int i = 0; i < n; ++i) {
-            while (!st.empty() && arr[st.top()] > arr[i]) {
+            while (!st.empty() && arr[st.top()] < arr[i]) {
                 int top = st.top();
-                int h = arr[top];
-                int w = i - top;
-                ans = std::max(ans, h * w);
                 st.pop();
                 if (st.empty()) {
                     break;
                 }
+                int left = st.top();
+                int h = std::min(arr[left], arr[i]) - arr[top];
+                int w = i - left - 1;
+                ans +=  h * w;
             }
             st.push(i);
         }
@@ -53,12 +61,11 @@ int maxAreaV2(std::vector<int>& arr) {
 }
 
 int main() {
-    std::vector<int> arr{ 4, 1, 2, 3, 15, 6};
+    std::vector<int> arr{ 4, 4, 4, 2, 10, 2, 2, 2,};
     int res = LargestRectangleArea(arr);
     printf("func1:%d\n", res);
 
-    res = maxAreaV2(arr);
+    res = largestWater(arr);
     printf("func2:%d\n", res);
-
     return 1;
 }
