11a9fede2c39605abbded5451984ab17478603d5
bo.liu <bliuredhat@gmail.com>
2025-04-18 16:18:23 +0800

    堆排序

--- 18_Base_Algorithm.cpp
+++ 18_Base_Algorithm.cpp
@@ -2,7 +2,7 @@
  * @Author: bliuredhat@gmail.com
  * @Date: 2025-04-18 12:11:06
  * @LastEditors: bliuredhat@gmail.com
- * @LastEditTime: 2025-04-18 14:56:08
+ * @LastEditTime: 2025-04-18 16:07:45
  * @Description: 
  */
 #include <vector>
@@ -57,10 +57,44 @@ void quicksort(std::vector<int>& vec) {
     quick_sort_handle(vec, 0, vec.size()-1);
 }
 
+//堆排序
+void head_adjust(std::vector<int>& vec, int root, int size){
+    int val = vec[root];
+    int children = 2* root +1;
+    for(; children < size; children = children*2+1) {
+        if (children+1 < size && vec[children] < vec[children+1]) {
+            children += 1;
+        }
+        if (vec[children] > val) {
+            std::swap(vec[children], val);
+        } else {
+            break;
+        }
+    }
+    vec[root] = val;
+}
+
+
+void heapSort(std::vector<int>& vec) {
+    int size = vec.size() ;
+    for (int root=size/2; root >= 0; root--) {
+        head_adjust(vec, root, size);
+    }
+    for (int idx=size-1; idx >= 0; idx--) {
+        std::swap(vec[0], vec[idx]);
+        head_adjust(vec, 0, idx);
+    }
+}
+
 int main() {
     std::vector<int> vec{8,3,7,1,6,2,7,4, 5};
     printVec(vec);
+    std::cout << "快速排序：" << std::endl;
     quicksort(vec);
+    printVec(vec);
+    std::cout << "堆排序：" <<std::endl;
+    std::vector<int> vec1{8,3,7,1,6,2,7,4, 5};
+    heapSort(vec);
     printVec(vec);
 }
 
